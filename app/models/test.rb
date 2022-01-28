f294881675a5ede933ca4d555a13d3c7422027b6
Ваше Имя <darhanbek89@gmail.com>
2022-01-28 23:50:16 +0600

    task5

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  belongs_to :author, class_name: :User, foreign_key: :author_id
+
   def self.sort_test_name_by_category(title)
     Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: {title: title}).order(title: :desc).pluck(:title)
   end
 end
- 
-#Создайте метод класса в модели Test, который возвращает отсортированный по убыванию массив названий всех Тестов
-#у которых Категория называется определённым образом 
-#(название категории передается в метод в качестве аргумента).
--- db/migrate/20220128104223_create_tests.rb
+++ db/migrate/20220128104223_create_tests.rb
@@ -4,6 +4,8 @@ class CreateTests < ActiveRecord::Migration[7.0]
       t.string :title, null: false
       t.integer :level, default: 0, null: false
       t.references :category, foreign_key: true
+      t.references :author, foreign_key: true
+
 
       t.timestamps
     end
