99ca3fe0ba78f88e644465e950cad367587393b6
Ваше Имя <darhanbek89@gmail.com>
2022-01-31 15:32:33 +0600

    drop_results_table

---
+++ db/migrate/20220131092508_drop_results_table.rb
@@ -0,0 +1,5 @@
+class DropResultsTable < ActiveRecord::Migration[7.0]
+  def change
+    drop_table :results
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_095043) do
+ActiveRecord::Schema.define(version: 2022_01_31_092508) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -33,16 +33,6 @@ ActiveRecord::Schema.define(version: 2022_01_30_095043) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
-  end
-
-  create_table "results", force: :cascade do |t|
-    t.boolean "status"
-    t.integer "user_id"
-    t.integer "test_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_results_on_test_id"
-    t.index ["user_id"], name: "index_results_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -70,8 +60,6 @@ ActiveRecord::Schema.define(version: 2022_01_30_095043) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "results", "tests"
-  add_foreign_key "results", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users", column: "author_id"
 end
